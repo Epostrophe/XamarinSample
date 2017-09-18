3a46d01156bd95542c2fe893ca0c7f150aee4b1f
0758692 <0758692@KELCE-ACIS19-P>
2017-09-18 17:32:25 -0500

    classes

---
+++ XamarinSample/XamarinSample/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinSample.Models
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string EmailAdress { get; set; }
+        public string Password { get; set; }
+
+        public List<vehicle> vehicles {get;set;}
+
+        public User()
+        {
+            this.vehicles = new List<vehicle>();
+        }
+        public void Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Authenticate()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ XamarinSample/XamarinSample/Models/vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinSample.Models
+{
+    class vehicle
+    {
+        public int ID { get; set; }
+        public string model { get; set; }
+        public string Make { get; set; }
+
+        public void LoadVehicleData(string vehicleID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
