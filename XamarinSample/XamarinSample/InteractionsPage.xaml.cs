cf3490923724cf328cc18fcf34f03b9d97a53667
0758692 <0758692@KELCE-ACIS20-P>
2017-09-20 17:42:07 -0500

    added adding

---
+++ XamarinSample/XamarinSample/InteractionsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InteractionsPage : ContentPage
+    {
+        public InteractionsPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void interactionButton_Clicked(object sender, EventArgs e)
+        {
+            decimal result = Convert.ToDecimal(firstnumberEntry.Text) + Convert.ToDecimal(secondnumberEntry.Text);
+
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
--- XamarinSample/XamarinSample/MainPage.xaml.cs
+++ XamarinSample/XamarinSample/MainPage.xaml.cs
@@ -18,5 +18,10 @@ namespace XamarinSample
         {
             Navigation.PushAsync(new Assignment2());
         }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new InteractionsPage());
+        }
     }
 }
